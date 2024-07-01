f4f8e95b6a545fb8abc82ea64a8607819d55e3a7
obrn544 <oobregon544@gmail.com>
2024-06-30 21:23:07 -0500

    configure .env

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,6 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=ARofeIafb3Nm9Js1ChWen045sif7VQGn&q=${category}&limit=10`;
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
